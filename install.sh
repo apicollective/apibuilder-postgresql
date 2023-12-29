dd0d697098e7fb8c3518bf4ceb18c11beecf3360
Michael Bryzek <mbryzek@alum.mit.edu>
2023-12-29 12:40:34 -0800

    Make api user owner

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 psql -U postgres -c 'create database apibuilderdb' postgres
 psql -U postgres -c 'create role api login PASSWORD NULL' postgres > /dev/null
 psql -U postgres -c 'GRANT ALL ON DATABASE apibuilderdb TO api' postgres
+psql -U postgres -c 'ALTER DATABASE apibuilderdb owner to api' postgres
 sem-apply --url postgresql://api@localhost/apibuilderdb
 
