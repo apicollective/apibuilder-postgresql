2436e74ccb06866a776a5c8172b5d283364e5918
Nicola Girardi <nicola.girardi@global-e.com>
2024-03-01 15:39:05 +0000

    FDN-2062: Add support for Postgres 15
    The install script needs to grant permissions to the public schema
    in Postgres 15.
    
    An additional `Dockerfile-15` has been added to create a Postgres 15
    based image. The Travis configuration has been updated to build that
    image.

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ psql -U postgres -c 'create database apibuilderdb' postgres
 psql -U postgres -c 'create role api login PASSWORD NULL' postgres > /dev/null
 psql -U postgres -c 'GRANT ALL ON DATABASE apibuilderdb TO api' postgres
 psql -U postgres -c 'ALTER DATABASE apibuilderdb owner to api' postgres
+psql -U postgres -c 'grant all on schema public to api' apibuilderdb
 sem-apply --url postgresql://api@localhost/apibuilderdb
 
